3a5921319ce1b6eea71aad2df1aece3be7056ea9
DDimitrova90 <ddimitrova90@gmail.com>
2017-08-15 18:11:20 +0300

    Added text in GameInfoLabel

--- Supermarket.Client/Forms/InfoMenuForm.Designer.cs
+++ Supermarket.Client/Forms/InfoMenuForm.Designer.cs
@@ -59,6 +59,7 @@
             // 
             // CurrentMoneyText
             // 
+            this.CurrentMoneyText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.CurrentMoneyText.Location = new System.Drawing.Point(12, 92);
             this.CurrentMoneyText.Multiline = true;
             this.CurrentMoneyText.Name = "CurrentMoneyText";
