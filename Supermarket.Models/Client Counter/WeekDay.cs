cb17ba7e7fe1b3e5766300b9e5917088c2a1a2fc
ChrisPam <chrispam1509@gmail.com>
2017-08-17 13:37:30 +0300

    Created enumerator

---
+++ Supermarket.Models/Client Counter/WeekDay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket.Models.Client_Counter
+{
+    public enum WeekDay
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6,
+        Sunday = 7
+    }
+}
